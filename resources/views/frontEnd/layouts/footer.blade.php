2c071f989621bef180372e70028d2970d6a3b81a
kayroeuyl <roeuykaykaya@gmail.com>
2020-12-18 08:53:01 +0700

    footer

--- resources/views/frontEnd/layouts/footer.blade.php
+++ resources/views/frontEnd/layouts/footer.blade.php
@@ -2,7 +2,7 @@
    
         <div class="container">
             <div class="row text-white">
-                <div class="col-lg-5 col-ml-5 col-md-5 col-sm-5">
+                <div class="col-lg-4 col-ml-5 col-md-5 col-sm-5">
                     <div class="single-widget">
                         <h2 >About Us</h2>
                         <p class="nav nav-pills nav-stacked">
@@ -16,10 +16,12 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-lg-3 col-ml-3 col-md-3 col-sm-3">
+                <div class="col-lg-4 col-ml-3 col-md-3 col-sm-3">
                     <div class="single-widget">
                         <h2>Term and Conditions</h2>
-                        
+                        <div class="col-lg-4 col-ml-4 col-md-4 col-sm-4"><p>Delivery</p></div>
+                        <div class="col-lg-1 col-ml-1 col-md-1 col-sm-1"><p>:</p></div>
+                        <div class="col-lg-7 col-ml-7 col-md-7 col-sm-7"><p>Free Phnom Penh City, Cambodia</p></div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-ml-4 col-md-4 col-sm-4">
